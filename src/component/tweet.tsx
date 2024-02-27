5c094ab005b59ca955b35745feec2db5b35f28b1
Kan <secureis@gmail.com>
2024-02-28 04:15:12 +0900

    Add Firebase integration and implement tweet deletion

--- src/component/tweet.tsx
+++ src/component/tweet.tsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 import { ITweet } from "./timeline";
+import { auth, db, storage } from "../firebase";
+import { deleteDoc, doc } from "firebase/firestore";
+import { deleteObject, ref } from "firebase/storage";
 
 const Wrapper = styled.div`
 	display:grid;
@@ -10,8 +13,9 @@ const Wrapper = styled.div`
 `;
 
 const Column = styled.div`
-	display:flex;
-	flex-direction:column;
+  &:last-child {
+    place-self: end;
+  }
 `;
 
 const Photo = styled.img`
@@ -25,18 +29,53 @@ const Username = styled.span`
 	font-size:15px;
 `;
 
-const Payload = styled.span`
+const Payload = styled.p`
 	margin: 10px 0px;
 	font-size: 18px;
 `;
 
+const ModifyButton = styled.button``;
+
+const DeleteButton = styled.button`
+	background-color: tomato;
+	color:white;
+	font-weight:600;
+	border:0;
+	font-size:12px;
+	padding: 5px 10px;
+	text-transform:uppercase;
+	border-radius:5px;
+	cusor:pointer;
+`;
+
+
+
 // ITweet 인터페이스를 받아와서 사용함
-export default function Tweet({username, photo, tweet}:ITweet){
+// timeline에서 Tweet를 랜더링할때, ITweet를 받아와서 사용함
+export default function Tweet({username, photo, tweet, userId, id}:ITweet){
+	const user = auth.currentUser;
+	const onDelete = async() => {
+		const ok = confirm("Are you sure you want to delete this tweet?");
+		if(!ok || user?.uid !== userId) return;
+
+		try{
+			await deleteDoc(doc(db, "tweets", id));
+			if(photo){
+				const photoRef = ref(storage, `tweets/${user.uid}/${id}`);
+				await deleteObject(photoRef);
+			}
+		}catch(e){
+			console.log(e);
+		}finally{
+			// 삭제
+		}
+	};
 	return (
 		<Wrapper>
 			<Column>
 				<Username>{username}</Username>
 				<Payload>{tweet}</Payload>
+				{user?.uid === userId ? <DeleteButton onClick={onDelete}>Delete</DeleteButton> : null}				
 			</Column>
 			<Column>
 				{photo? (<Photo src={photo} />) : null}
--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,3 +1,135 @@
+import styled from "styled-components";
+import {auth, db, storage} from "../firebase";
+import { useEffect, useState } from "react";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { updateProfile } from "firebase/auth";
+import { collection, getDocs, limit, orderBy, query, where } from "firebase/firestore";
+import { ITweet } from "../component/timeline";
+import { set } from "firebase/database";
+import Tweet from "../component/tweet";
+
+const Wrapper = styled.div`
+	display:flex;
+	align-items:center;
+	flex-direction:column;
+	gap:20px;
+`;
+
+const AvatarUpload = styled.label`
+	width:80px;
+	overflow:hidden;
+	height:80px;
+	border-radius:50%;
+	background-color: #1d9bf0;
+	cursor:pointer;
+	display:flex;
+	justify-content:center;
+	align-items:center;
+	svg{
+		width:50px;
+		fill:white;
+	}
+`;
+
+const AvatarImg = styled.img`
+	width:100%;
+`;
+
+const AvatarInput = styled.input`
+	display:none;
+
+`;
+
+const Name = styled.span`
+	font-size:22px;
+`;
+
+const Tweets = styled.div`
+	display:flex;
+	width:100%;
+	flex-direction:column;
+	gap:20px;
+`;
+
+
 export default function Profile(){
-	return <h1>Profile</h1>
+	const user = auth.currentUser;
+	const [avatar, setAvatart] = useState(user?.photoURL);
+	const onAvatarChange = async(e:React.ChangeEvent<HTMLInputElement>) => {
+		const {files} = e.target;
+		if(!user) return;
+
+		if(files && files.length === 1){
+			const file = files[0];
+			const locationRef = ref(storage, `avatars/${user?.uid}`);
+			const result = await uploadBytes(locationRef, file)
+			const avatarUrl = await getDownloadURL(result.ref);
+			setAvatart(avatarUrl);
+
+			await updateProfile(user, {photoURL: avatarUrl});
+			// updateProfile는 firebase/auth에서 가져온 함수인데.. 왜 이렇게 사용하는지는 모르겠음
+		}
+		// 선택된 파일을 바로 서버로 업로드하는 코드로 avatars에 유저 아이디로 저장하는 것임.
+		// post-tweet-form.tsx에서도 파일을 업로드하는 코드가 있으니 참조하자.(96라인쯤)
+	};
+
+	const DefaultAvatar = () => (
+		<svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+			<path d="M10 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM3.465 14.493a1.23 1.23 0 0 0 .41 1.412A9.957 9.957 0 0 0 10 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 0 0-13.074.003Z" />
+		</svg>
+	);
+	// const defaultAvatar = '<svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"><path d="M10 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM3.465 14.493a1.23 1.23 0 0 0 .41 1.412A9.957 9.957 0 0 0 10 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 0 0-13.074.003Z" /></svg>';
+	// 이처럼 지정하면 스트링으로 인식해서 이미지가 나오지 않고, 코드형태로 출력됨
+	// 그래서 React 컴포넌트로 만들어서 사용했음
+
+
+
+	const [tweets, setTweets] = useState<ITweet[]>([]);
+	// useState로 tweets를 ITweet배열로 지정하고, 기본값은 빈배열로 지정함
+	// 다시 말해 이것들은 배열로 호출되는 인터페이스를 갖게 될것이고, 빈 배열로 시작한다는 뜻이다.
+
+	//자신의 tweet를 가져오기
+	const fetchTweets = async() => {
+		const tweetQuery = query(
+			collection(db, "tweets"),
+			where("userId", "==", user?.uid), // fireabse에서 index를 생성해 줘야 한다.
+			orderBy("createdAt", "desc"),
+			limit(25)
+		);
+
+		const snapshot = await getDocs(tweetQuery);
+		const tweets = snapshot.docs.map(doc=>{
+			const {tweet, createdAt, userId, username, photo} = doc.data();
+			return {
+				id: doc.id,
+				photo,
+				tweet,
+				userId,
+				username,
+				createdAt,
+			};
+		});
+		setTweets(tweets);
+	};
+
+	useEffect(()=>{
+		fetchTweets();
+	}, []);
+
+
+	return (
+		<Wrapper>
+			<AvatarUpload htmlFor="avatar">
+				{avatar ? (<AvatarImg src={avatar}/>) : (<DefaultAvatar />)}
+			</AvatarUpload>
+			<AvatarInput onChange={onAvatarChange} id="avatar" type="file" accept="image/*"/>
+			<Name>
+				{user?.displayName?? "Anonymous"}
+				{/* {user?.displayName?user.displayName: "Anonymous"} 를 축약한 것임 */}				
+			</Name>
+			<Tweets>
+				{tweets.map(tweet=><Tweet key={tweet.id} {...tweet}/>)}
+			</Tweets>
+		</Wrapper>
+	);
 }
